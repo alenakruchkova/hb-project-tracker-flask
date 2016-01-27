8af43d5d002f6d6ff6c699a55a618f38bfaa3d96
Hackbright Student <info@hackbrightacademy.com>
2016-01-27 13:01:38 -0800

    incomplete

--- hackbright-web.py
+++ hackbright-web.py
@@ -4,6 +4,33 @@ import hackbright
 
 app = Flask(__name__)
 
+@app.route("/student_add")
+def student_add():
+    """Add a student."""
+
+    return render_template("new_student.html")
+
+
+
+@app.route("/new_student_confirmation", methods=['POST'])
+def new_student_confirm():
+    """Confirms that new student has been registered and provides link to student page"""
+
+    first_name = request.form.get("firstname")
+    last_name = request.form.get("lastname")
+
+    return render_template("new_student_confirmation.html",
+                            fname=first_name,
+                            lname=last_name)
+
+
+@app.route("/student_search")
+def get_student_form():
+    """Show form for searching for a student."""
+
+    return render_template("student_search.html")
+
+
 
 @app.route("/student")
 def get_student():
@@ -12,16 +39,12 @@ def get_student():
     github = request.args.get('github', 'jhacks')
     first, last, github = hackbright.get_student_by_github(github)
     html = render_template("student_info.html",
-                            first=first,
-                            last=last,
-                            github=github)
+                           first=first,
+                           last=last,
+                           github=github)
     return html
 
-@app.route("/student_search")
-def get_student_form():
-    """Show form for searching for a student."""
 
-    return render_template("student_search.html")
 
 
 
